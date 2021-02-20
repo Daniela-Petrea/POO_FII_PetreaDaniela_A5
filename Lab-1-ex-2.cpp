260e69497a29d67c74d02f92507b64c521c1c691
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-02-20 14:53:13 +0200

    Update Lab-1-ex-2.cpp

--- Lab-1-ex-2.cpp
+++ Lab-1-ex-2.cpp
@@ -1,29 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 using namespace std;
-
+int functie(char myString[])
+{
+    int nr = 0, i = 0;
+    while (myString[i] != '\0')
+    {
+        nr *= 10;
+        nr += (myString[i] - '0');
+        i++;
+    }
+    return nr;
+}
 int main()
 {
-    int n=0;
-FILE* fp;
-if (fopen_s(&fp, "in.txt", "r") != 0)
-{
-    printf("Eroare. Nu am putut deschide fisierul\n");
-}
-else {
-    printf("Am deschis fisierul cu success!\n");
-    char myString[200];
-        while (fgets(myString, 200, fp))
-        {
-            myString[strlen(myString)] = '\0';
-            printf("Am citit din fisier: %s\n", myString);
-            n = n + atoi(myString);
-        }
+    int n = 0;
+    FILE* fp = fopen("in.txt", "r");
+    while (feof(fp) == 0)
+    {
+        char myString[10] = "\0";
+        fscanf(fp, "%s", myString);
+        n = functie(myString) + n;
     }
-printf("Valoarea lui n este: %d\n", n);
-system("pause");
-return 0;
+    printf("Valoarea lui n este: %d\n", n);
+    system("pause");
+    return 0;
 }
