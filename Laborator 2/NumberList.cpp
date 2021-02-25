71523d73bfee1417fdb90a14b3b10feb76471a30
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-02-25 16:45:41 +0200

    Update NumberList.cpp

--- Laborator 2/NumberList.cpp
+++ Laborator 2/NumberList.cpp
@@ -1,19 +1,52 @@
 #include "NumberList.h"
+#include <iostream>
 #include <string.h>
 #include <stdio.h>
-//aici vom implementa metodele din clasa
-//ca sa implementam o metoda din clasa 
-//<tipuldereturn> <nume_clasa>::<nume_metoda> (<parametri deca sunt>) {<implementarea>}
-//ca sa accesam atribute ale clasei folosim this->
+using namespace std;
 
-//constructorul si destructorul nu au valori de return
+void NumberList::Init()
+{
+	this->count = 0;
+}
+bool NumberList::Add(int x)
+{
+	this->numbers[count] = x;
+	this->count++;
+	if(this->count<10)
+	return true;
+	else
+	return false;
+}
+void NumberList::Sort()
+{
+	int schimb;
+	for (int i = 0; i <= this->count - 2; i++)
+	{
+		for (int j = i + 1; j <= this->count - 1; j++)
+		{
+			if (this->numbers[i] > this->numbers[j])
+			{
+				schimb = this->numbers[i];
+				this->numbers[i] = this->numbers[j];
+				this->numbers[j] = schimb;
+			}
+		}
 
+	}
+}
+void NumberList::Print()
+{
+	for (int index = 0; index < this->count; index++)
+		cout<<this->numbers[index]<<" ";
+	cout << endl;
+}
 
 NumberList::NumberList() {
-	printf("S-a apelat constructorul pentru obiectul meu\n");
+	printf("S-a apelat constructorul\n");
 	this->count = 0;
 }
 
 NumberList::~NumberList() {
 	printf("S-a apelat destructorul\n");
 }
+
