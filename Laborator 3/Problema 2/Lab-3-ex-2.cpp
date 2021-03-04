068f9bf8fdb4aa4a8995ac3ff72563d35767bc70
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-03-04 17:24:34 +0200

    Update Lab-3-ex-2.cpp

--- Laborator 3/Problema 2/Lab-3-ex-2.cpp
+++ Laborator 3/Problema 2/Lab-3-ex-2.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include "Canvas.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    Canvas desen(20, 20);
+    desen.DrawCircle(5, 5, 6, '*');
+   desen.Print();
+    desen.FillCircle(5, 5, 6, '^');
+    desen.Print();
+    desen.DrawRect(13, 3, 17, 8, '*');
+    desen.Print();
+    desen.FillRect(13, 3, 17, 8, '&');
+    desen.SetPoint(15, 1, 'k');
+    desen.DrawLine(15, 3, 15, 10, 'd');
+    desen.Print();
+    desen.Clear();
+    desen.Print();
+    system("pause");
+    return 0;
 }
+
+
 
 
