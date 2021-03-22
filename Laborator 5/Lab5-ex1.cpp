23bf9f32c5eacb9da2830926bea29fd010eb5706
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-03-22 17:27:59 +0200

    Update Lab5-ex1.cpp

--- Laborator 5/Lab5-ex1.cpp
+++ Laborator 5/Lab5-ex1.cpp
@@ -7,20 +7,15 @@ using namespace std;
 int main()
 
 {
-
     Number n1("10110010", 2);
-
     Number n2("734", 8);
-
     Number n3("FF", 16);
-
-
-
     printf("n1 has %d digits and it is written in base %d\n", n1.GetDigitsCount(), n1.GetBase());
     printf("n2 has %d digits and it is written in base %d\n", n2.GetDigitsCount(), n2.GetBase());
     printf("n3 has %d digits and it is written in base %d\n", n3.GetDigitsCount(), n3.GetBase());
     n1.Print();
     n2.Print();
     n3.Print();
-
+    n3.SwitchBase(2);
+    n3.Print();
 }
