6c40df4101cc0f327e2a870c2b754eab4b0a79f5
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-03-22 17:28:48 +0200

    Update Number.h

--- Laborator 5/Number.h
+++ Laborator 5/Number.h
@@ -6,15 +6,14 @@ class Number
 {
 	int baza;
 	char* valoare;
-
+	int conversie(char *v,int b);
+	
 public:
 
 	Number(const char* value, int base); // where base is between 2 and 16
 
 	~Number();
-
-	//int operator[](int index);
-
+    
 	void SwitchBase(int newBase);
 
 	void Print();
