99d4700ca0685f272878f5974195671c9ce8e8fa
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-03-27 15:21:38 +0200

    Update Ford.h

--- Laborator 6/Ford.h
+++ Laborator 6/Ford.h
@@ -1,15 +1,20 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
-class Ford :
-	public Car
+#include "Car.h"
+class Ford : public Car
 {
-	int fuelcapacity;
-	int fuelconsmption;
-	int rainspeed;
-	int sunnyspeed;
-	int snowspeed;
 public:
 	Ford();
 	~Ford();
+	char* getcarname();
+	int getfuelcapacity();
+	int getfuelconsumption();
+	int getrainspeed();
+	int getsunnyspeed();
+	int getsnowspeed();
+	void setracetime(double time);
+	double getracetime();
+	void setraceconsumption(double consumare);
+	double getraceconsumption();
 };
 
