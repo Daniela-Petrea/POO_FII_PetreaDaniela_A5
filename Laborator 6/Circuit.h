406a2431d33aacb6324cddcd8ae3797703a214ed
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-03-27 15:20:03 +0200

    Update Circuit.h

--- Laborator 6/Circuit.h
+++ Laborator 6/Circuit.h
@@ -1,22 +1,24 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
 #include "Car.h"
-#include <stdlib.h>
+#include "Weather.h"
+
 class Circuit
 {
 	Car** car;
 	int currentIndex;
 	int maxIndex;
-	int lungime;
+	int weather;
+	double lungime;
 public:
 	Circuit();
 	~Circuit();
-	int SetLength(int length);
-	//int SetWeather;
+	void SetLength(double length);
+	void SetWeather(Weather set_weather);
 	bool AddCar(Car* pointer);
 	void Race();
 	void ShowFinalRanks(); 
 	void ShowWhoDidNotFinish(); 
-
 };
+
 
