a8792324a203fd1e280db05e064efe7a156e87ea
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-03-27 15:27:57 +0200

    Update Lab-6-ex-1.cpp

--- Laborator 6/Lab-6-ex-1.cpp
+++ Laborator 6/Lab-6-ex-1.cpp
@@ -1,25 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-#include "Car.h"
 #include "Mazda.h"
 #include "Ford.h"
 #include "Dacia.h"
 #include "Toyota.h"
 #include "Mercedes.h"
-#include "Weather.h"
 #include "Circuit.h"
 
 int main()
 
 {
-
 	Circuit c;
 
 	c.SetLength(100);
 
-	//c.SetWeather(Weather::Rain);
+	c.SetWeather(Weather::Rain);
 
 	c.AddCar(new Dacia());
 
@@ -33,7 +26,7 @@ int main()
 
 	c.Race();
 
-	c.ShowFinalRanks(); // it will print the time each car needed to finish the circuit sorted from the fastest car to the   slowest.
+	c.ShowFinalRanks(); // it will print the time each car needed to finish the circuit sorted from the fastest car to the slowest.
 
 	c.ShowWhoDidNotFinish(); // it is possible that some cars don't have enough fuel to finish the circuit
 
