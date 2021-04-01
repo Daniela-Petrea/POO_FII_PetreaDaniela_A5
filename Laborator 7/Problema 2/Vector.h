83c66556178f473264763700e078f1f3b30090d9
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-04-01 14:00:24 +0300

    Update Vector.h

--- Laborator 7/Problema 2/Vector.h
+++ Laborator 7/Problema 2/Vector.h
@@ -5,15 +5,18 @@ using namespace std;
 template <typename T, int size>
 class Vector
 {
-	T vector[size];
-	T variabila=0;
+	T* vector;
+	T variabila = 0;
 public:
+	
 	T Push(T myvar)
-	{
+	{   if(this->variabila==0)
+		vector= (int*)calloc(size , sizeof(int));
 		if (this->variabila < size)
 		{
 			this->vector[this->variabila] = myvar;
-			this->variabila++;	
+			this->variabila++;
+			vector = (int*)(realloc(vector, size * sizeof(int)));
 		}
 		return this->vector[this->variabila];
 	}
@@ -27,45 +30,45 @@ public:
 	}
 	void metod(T myvalue, T myindex)
 	{
-			if (myindex < 0 || myindex > this->variabila)
-		{
-			cout << "Invalid\n";
-		}
-			else
-			{
-				this->vector[myindex] = myvalue;
-			}
-	}
-	const void get()
-	{
-		for (int i = 0; i < this->variabila ; i++)
-			cout<< &vector[i]<<" ";
-	}
-	void remove(T myindex)
-	{
-		if (myindex < 0 || myindex > this->variabila)  
+		if (myindex < 0 || myindex > this->variabila)
 		{
 			cout << "Invalid\n";
 		}
 		else
 		{
-			for (int i = myindex; i <= this->variabila-1 ; i++)
+			this->vector[myindex] = myvalue;
+		}
+	}
+	const void get()
+	{
+		for (int i = 0; i < this->variabila; i++)
+			cout << &vector[i] << " ";
+	}
+	void remove(T myindex)
+	{
+		if (myindex < 0 || myindex > this->variabila)
+		{
+			cout << "Invalid\n";
+		}
+		else
+		{
+			for (int i = myindex; i < this->variabila ; i++)
 			{
-				this->vector[i] = this->vector[i + 1];			
+				this->vector[i] = this->vector[i + 1];
 			}
 			this->variabila--;
 		}
 	}
-	void insert(T myvalue,T myindex)
+	void insert(T myvalue, T myindex)
 	{
 		int i;
 		this->variabila++;
-		
-		for (i = this->variabila - 1; i >=myindex+1;i--)
+
+		for (i = this->variabila - 1; i >= myindex + 1; i--)
 		{
 			vector[i] = vector[i - 1];
 		}
-		vector[myindex ] = myvalue;
+		vector[myindex] = myvalue;
 	}
 	void print()
 	{
@@ -73,25 +76,8 @@ public:
 			cout << this->vector[index] << " ";
 		cout << endl;
 	}
-	void firstIndexOf(T& obiectdecautat, bool (*isEqual) (T o1, T o2) = nullptr)
-	{
-		for(int i=0;i<size;i++)
-		{
-			if (isEqual != nullptr)
-			{
-				if (isEqual(obiectdecautat, this->vector[i]))
-					cout<< i<<endl;
-			}
-			else
-			{
-				if(obiectdecautat == this->vector[i])
-					cout<< i<<endl;
-			}
-		}
-	}
 	void sort(bool (*issort) (T o1, T o2) = nullptr)
 	{
-		int schimb;
 		for (int i = 0; i <= this->variabila - 2; i++)
 		{
 			for (int j = i + 1; j <= this->variabila - 1; j++)
@@ -100,30 +86,44 @@ public:
 				{
 					if (issort(this->vector[i], this->vector[j]))
 					{
-						schimb = this->vector[i];
-						this->vector[i] = this->vector[j];
-						this->vector[j] = schimb;
+						swap(vector[i], vector[j]);
 					}
 				}
 				else
 				{
 					if (this->vector[i] > this->vector[j])
 					{
-						schimb = this->vector[i];
-						this->vector[i] = this->vector[j];
-						this->vector[j] = schimb;
+						swap(vector[i], vector[j]);
 					}
 				}
 			}
 
 		}
 	}
+	T firstIndexOf(T obiectdecautat, bool (*isEqual) (T o1, T o2) = nullptr)
+	{
+		for (int i = 0; i < this->variabila; i++)
+		{
+			if (isEqual != nullptr)
+			{
+				if (isEqual(obiectdecautat, this->vector[i]))
+					return i;
+			}
+			else
+			{
+				if (obiectdecautat == this->vector[i])
+					return i;
+			}
+		}
+		return -1;
+	}
 };
 
 bool equal(int i1, int i2)
 {
-		return i1 == i2;
-	}
+	return i1 == i2;
+}
+
 
 bool issorted(int i1, int i2)
 {
