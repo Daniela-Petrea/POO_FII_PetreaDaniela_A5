b25e7e5c065550d1cf491dbda2894cca38a0335a
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2021-05-23 16:04:41 +0300

    Update Agenda.h

--- Laborator 12/Agenda.h
+++ Laborator 12/Agenda.h
@@ -10,13 +10,11 @@ class Agenda
 public:
 	void addContact(Contact*);	
 	string getName();
-	//string getNume();
-	//void setData(string data);
-	//string getData();
-	//void setNr_telefon(string nr_telefon);
-	//string getNr_telefon();
-   // void setAdresa(string adresa);
-	//string getAdresa();
-	
+	void setData(string data);
+	string getData();
+	void setNr_telefon(string nr_telefon);
+	string getNr_telefon();
+        void setAdresa(string adresa);
+	string getAdresa();
 	void printList();
 };
